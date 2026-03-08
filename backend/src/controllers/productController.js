a0dec908774271821ac1731b33841d63226fc2ff
anil <anil.anilkumarchanvan76@gmail.com>
2026-03-08 17:04:26 +0530

    feat:  checking api test for products

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,114 @@
+const Product = require("../models/productModel");
+
+const createProduct = async (req, res) => {
+  try {
+    const { name, sku, category, price, stock, barcode, description } = req.body;
+
+    const product = new Product({
+      name,
+      sku,
+      category,
+      price,
+      stock,
+      barcode,
+      description,
+    });
+
+    const savedProduct = await product.save();
+
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+const searchProducts = async (req, res) => {
+  try {
+
+    const { keyword = "", page = 1, limit = 10 } = req.query;
+
+    const query = {
+      name: { $regex: keyword, $options: "i" }
+    };
+
+    const products = await Product.find(query)
+      .limit(limit * 1)
+      .skip((page - 1) * limit);
+
+    const count = await Product.countDocuments(query);
+
+    res.status(200).json({
+      totalProducts: count,
+      currentPage: page,
+      totalPages: Math.ceil(count / limit),
+      products
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  searchProducts
+};
