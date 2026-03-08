d9e0b6f429cdd3796799898dd263d8ecd5edb6c4
anil <anil.anilkumarchanvan76@gmail.com>
2026-03-08 17:05:10 +0530

    feat: pashword hasing and jwt token verification

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  try {
+
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+
+    next();
+
+  } catch (error) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: "Access denied"
+      });
+    }
+
+    next();
+  };
+};
+module.exports = {
+  protect,
+  authorizeRoles
+};
