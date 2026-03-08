a1b973a87a85709e324dabb822d07ff3bc8775d1
anil <anil.anilkumarchanvan76@gmail.com>
2026-03-08 17:08:25 +0530

    feat: server implemented

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("POS API Running");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
